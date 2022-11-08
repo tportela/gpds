517204c441243ba670c5e6b7b8e1f7720b76c338
tportela <tportela@uts.edu.mx>
2022-11-08 15:46:39 -0700

    Delete hola.js

--- hola.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-
-var manejador = function(solicitud, respuesta){
-    console.log("Hola mundo");
-    respuesta.end();
-};
-var servidor = http.createServer(manejador);
-servidor.listen(8000);
